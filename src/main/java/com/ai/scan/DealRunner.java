8cadd79c372d11d7e189aab4530968a67f55f0d0
wutb <wutb@asiainfo.com>
2016-11-03 16:29:08 +0800

    fix a bug: task exception will supress subquence excutions

--- src/main/java/com/ai/scan/DealRunner.java
+++ src/main/java/com/ai/scan/DealRunner.java
@@ -7,41 +7,42 @@ import org.slf4j.LoggerFactory;
 
 import redis.clients.jedis.Jedis;
 
-public class DealRunner implements Runnable{
-	private static final Logger LOG=LoggerFactory.getLogger(DealRunner.class);
-	private int index=0;
+public class DealRunner implements Runnable {
+	private static final Logger LOG = LoggerFactory.getLogger(DealRunner.class);
+	private int index = 0;
 	private ScanConfig config;
 	private IDealService dealService;
-	private volatile boolean startFlag=true;
-	
+	private volatile boolean startFlag = true;
 
 	public DealRunner(int i, ScanConfig config) {
-		this.index=i;
-		this.config=config;
-		this.dealService=config.getDealService();
+		this.index = i;
+		this.config = config;
+		this.dealService = config.getDealService();
 	}
 
 	@Override
 	public void run() {
-		Jedis jedis=this.config.getJedisPool().getResource();
-		//jedis监视一个队列
-		while(startFlag){
-			try{
-				List<String> pushs=jedis.brpop(this.config.getBlockTimeout(), this.config.getQueueKey()+":"+this.index);
-				if(pushs!=null){
-					LOG.debug("线程"+pushs.get(0)+"处理推送消息:"+pushs.get(1));
+		Jedis jedis = null;
+		// jedis监视一个队列
+		while (startFlag) {
+			try {
+				if (jedis == null) {
+					jedis = this.config.getJedisPool().getResource();
+				}
+				List<String> pushs = jedis.brpop(this.config.getBlockTimeout(), this.config.getQueueKey() + ":" + this.index);
+				if (pushs != null) {
+					LOG.debug("线程" + pushs.get(0) + "处理推送消息:" + pushs.get(1));
 					Object record = dealService.decode(pushs.get(1));
 					dealService.deal(record);
 				}
-			}catch(Exception e){
-				e.printStackTrace();
+			} catch (Exception e) {
+				LOG.error("处理过程异常：", e);
 			}
 		}
-		
 	}
-	
-	public void shutdown(){
-		this.startFlag=false;
+
+	public void shutdown() {
+		this.startFlag = false;
 	}
 
 }
--- src/main/java/com/ai/scan/ScanRunner.java
+++ src/main/java/com/ai/scan/ScanRunner.java
@@ -10,35 +10,39 @@ import redis.clients.jedis.Jedis;
 
 import com.google.gson.Gson;
 
-public class ScanRunner implements Runnable{
-	private static final Logger LOG=LoggerFactory.getLogger(ScanRunner.class);
+public class ScanRunner implements Runnable {
+	private static final Logger LOG = LoggerFactory.getLogger(ScanRunner.class);
 	private ScanConfig config;
 	private IScanService service;
-	private Random random=new Random();
-	private Gson gson=new Gson();
+	private Random random = new Random();
+	private Gson gson = new Gson();
 
 	public ScanRunner(ScanConfig scanConfig) {
-		this.config=scanConfig;
-		this.service=scanConfig.getScanService();
+		this.config = scanConfig;
+		this.service = scanConfig.getScanService();
 	}
 
 	@Override
 	public void run() {
-		Jedis jedis=this.config.getJedisPool().getResource();
-		LOG.debug("扫描表记录开始");
-		List records=service.scan(Integer.valueOf(this.config.getFetchSize()));
-		LOG.debug("取到"+records.size()+"条记录");
-		for(Object record : records){
-			//更新状态
-			int row=service.updateStatus(record);
-			if(row == 1){
-				//简单随机分发
-				int index=random.nextInt(this.config.getPoolSize());
-				jedis.lpush(this.config.getQueueKey()+":"+index, gson.toJson(record));
+		try {
+			Jedis jedis = this.config.getJedisPool().getResource();
+			LOG.debug("扫描表记录开始");
+			List records = service.scan(Integer.valueOf(this.config.getFetchSize()));
+			LOG.debug("取到" + records.size() + "条记录");
+			for (Object record : records) {
+				// 更新状态
+				int row = service.updateStatus(record);
+				if (row == 1) {
+					// 简单随机分发
+					int index = random.nextInt(this.config.getPoolSize());
+					jedis.lpush(this.config.getQueueKey() + ":" + index, gson.toJson(record));
+				}
 			}
+
+			jedis.close();
+		} catch (Exception e) {
+			LOG.error("扫描表异常：", e);
 		}
-		
-		jedis.close();
 	}
 
 }
